bc35c8b26830f4cd87bc3f119b4d03b7e949401a
chris.garland <chris.garland@GLOBALSTRATEGIES.COM>
2020-03-03 17:02:34 -0800

    Part 8 - error w followers?

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table(
+	'followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+	)
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
@@ -13,7 +19,8 @@ class User(UserMixin, db.Model):
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 	followed = db.relationship(
-		'User': secondary=followers,
+		'User',
+		secondary=followers,
 		primaryjoin=(followers.c.follower_id == id),
 		secondaryjoin=(followers.c.followed_id == id),
 		backref=db.backref('followers', lazy='dynamic'),
@@ -48,6 +55,13 @@ class User(UserMixin, db.Model):
 			followers.c.followed_id == user.id
 		).count() > 0
 
+	def followed_posts(self):
+		followed = Post.query.join(
+			followers, (followers.c.followed_id == Post.user_id)).filter(
+				followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id=self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
+
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.String(140))
@@ -61,8 +75,3 @@ class Post(db.Model):
 def load_user(id):
 	return User.query.get(int(id))
 
-followers = db.Table(
-	'followers',
-	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-	)
